d22a731dcde3e31b2da170a7535c6c165a13ee37
dnogu <dnogueira85@gmail.com>
2022-09-15 10:53:14 -0400

    update

---
+++ variable.tf
@@ -0,0 +1,29 @@
+variable "rules" {
+  description = "Panorama API Key"
+  type = map(object({ app = string
+    source = object({
+      ip-netmask = list(string)
+      fqdn       = list(string)
+    })
+    destination = object({
+      ip-netmask = list(string)
+      fqdn       = list(string)
+      service    = list(string)
+    })
+  }))
+  nullable = false
+  default = {
+    samplerule = {
+      app = "test"
+      source = {
+        ip-netmask = ["10.10.10.10/32"]
+        fqdn       = []
+      }
+      destination = {
+        ip-netmask = ["10.10.10.10/32"]
+        fqdn       = []
+        service    = ["tcp/443", "tcp/80"]
+      }
+    },
+  }
+}
